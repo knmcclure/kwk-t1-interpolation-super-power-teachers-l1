e665bcf805346bacb5f4e39baecc459e670d2204
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-05-09 22:48:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,8 @@
 # Write your #display_rainbow method here
 rainbow = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I:indigo", "V: violet"]
+
 def display_rainbow(rainbow)
-  puts 
+  puts #{rainbow[0].slice(0)
 end
 
-display_rainbow
+display_rainbow(rainbow)
